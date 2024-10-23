afe9195ea6579cf7db1455626f19e068f50e6322
georgiy <kuman8443@gmail.com>
2024-10-23 21:33:45 +0300

    Аргументы и опции

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,6 +3,7 @@ parser = argparse.ArgumentParser(prog ='gendiff',
                         description='Compares two configuration files and shows a difference.')
 parser.add_argument('first_file', help='')
 parser.add_argument('second_file', help='')
+parser.add_argument('-f','--format', help = 'set format of output')
 
 def main():
     return parser.print_help()
